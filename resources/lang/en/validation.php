5a3dce54e294043dfa6f38c628ddbe9dcbb77792
Tri Nguyen <nguyenmtri11@gmail.com>
2023-11-17 09:54:59 +0700

    Add validation rule and translation for error messages

---
+++ resources/lang/en/validation.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'totp_code' => 'The Code is invalid or has expired.',
+    'email_code' => 'The Code is invalid or has expired.',
+    'sms_code' => 'The Code is invalid or has expired.',
+];
--- src/LaraMfaServiceProvider.php
+++ src/LaraMfaServiceProvider.php
@@ -2,9 +2,11 @@
 
 namespace Kohaku1907\LaraMfa;
 
+use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Routing\Router;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
+use Illuminate\Support\Facades\Validator;
 
 class LaraMfaServiceProvider extends PackageServiceProvider
 {
@@ -13,6 +15,7 @@ class LaraMfaServiceProvider extends PackageServiceProvider
         $package
             ->name('lara-mfa')
             ->hasConfigFile('mfa')
+            ->hasTranslations()
             ->hasMigrations(['create_lara_mfa_table', 'create_lara_mfa_settings_table']);
     }
 
